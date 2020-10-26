544cf13050c071329d888da8607e92e67d287737
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-26 18:47:03 +0200

    Updates

---
+++ precache-manifest.1f9316306531fd170eb8d9536459e155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee5988897af7327b0fb7105e4775901",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5fff3ca7b9d07850f9b",
+    "url": "./static/css/main.63d7c3f1.chunk.css"
+  },
+  {
+    "revision": "35e390cef9a8d982fcb0",
+    "url": "./static/js/2.f2bbea16.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.f2bbea16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5fff3ca7b9d07850f9b",
+    "url": "./static/js/main.6a96982c.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
